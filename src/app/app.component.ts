1e370df06a9276a1d2adea38ce3f5be783df8867
Luciano Murruni <luciano.murruni@conte.it>
2019-01-18 07:48:30 +0100

    Complete the open/close menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import {AfterViewInit, Component} from '@angular/core';
 import { trigger, transition, state, useAnimation, style } from '@angular/animations';
 import { fadeIn, fadeOut } from 'ng-animate';
 import {SideBarService} from './shared/services/sidebar.service';
+import { Router, Event, NavigationStart, NavigationEnd, NavigationError } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -21,8 +22,29 @@ export class AppComponent implements AfterViewInit {
   containerStatus = 'hidden';
 
   constructor(
-    public sidebarService: SideBarService
-  ) {}
+    public sidebarService: SideBarService,
+    private router: Router
+  ) {
+      router.events.subscribe( (event: Event) => {
+
+        if (event instanceof NavigationStart) {
+          // Show loading indicator
+          console.log('start');
+          this.sidebarService.toggleSidebarStatus(true);
+        }
+
+        if (event instanceof NavigationEnd) {
+          console.log('end');
+        }
+
+        if (event instanceof NavigationError) {
+          // Hide loading indicator
+
+          // Present error to user
+          console.log(event.error);
+        }
+    });
+  }
 
   ngAfterViewInit() {
     setTimeout(() => {
@@ -34,4 +56,5 @@ export class AppComponent implements AfterViewInit {
   openCloseSidebar() {
     this.sidebarService.toggleSidebarStatus();
   }
+
 }
