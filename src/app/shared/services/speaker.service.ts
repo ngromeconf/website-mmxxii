b9e3a9f28d6ed4c0496db9d64ef9f38f96c0d618
Luciano Murruni <luciano.murruni@conte.it>
2019-02-22 18:53:57 +0100

    new partner and speaker info

--- src/app/shared/services/speaker.service.ts
+++ src/app/shared/services/speaker.service.ts
@@ -37,7 +37,7 @@ export class SpeakerService {
       name: 'Ayşegül Yönet',
       job: '@azureadvocates, @GoogleDevExpert for Web Technologies',
       company: 'CodeForGoodWeek',
-      description: '@GoogleDevExpert for Web Technologies, #Angular. #WebVR and #Visualization, #NeuroTech bug. @CodeForGoodWeek, previously @nrwl_io, Autodesk',
+      description: 'Aysegul is a Azure Cloud Developer Advocate at Microsoft. Aysegul, focuses on Spatial Computing, Mixed Reality, Angular and Data Visualization. She is a Google Developer Expert for Web Technologies & Angular and an active conference speaker. She previously worked as Senior Software Engineer at Nrwl.io, Autodesk Play, a 3D and VR authoring tool and A360 Viewer and A360 Drive. She enjoys teaching at a variety of non-profit organizations aiming to increase diversity in the software industry. She is the co-founder of code4good, an organization with the goal of helping people to contribute to open source. Previously she led Women Who Code meetups, Girl Develop It workshops and was a CTO at AnnieCannons, a bootcamp dedicated to teach tech skills to human trafficking survivors.',
       visible: false
     },
     {
