776f4e9e5d7afbf30a5ea2f339dce6670abaee8b
Luciano Murruni <luciano.murruni@conte.it>
2019-05-19 20:24:10 +0200

    move animation to logo component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,10 +15,10 @@ import { Expo, Linear, TimelineLite, TweenLite } from "gsap";
       state('visible', style({ 'opacity': 1})),
       state('hidden', style({ 'opacity': 0})),
       transition('visible => hidden', useAnimation(fadeOut, {
-        params: { timing: 2, delay: 0.5 }
+        params: { timing: 0.1, delay: 0.1 }
       })),
       transition('hidden => visible', useAnimation(fadeIn, {
-        params: { timing: 0.5, delay: 0.1 }
+        params: { timing: 5, delay: 2 }
       })),
     ]),
   ]
@@ -73,11 +73,10 @@ export class AppComponent implements AfterViewInit {
   ngOnInit(){
     //check if is available a new version
     this.pwaService.checkUpdateAvailable();
-    this.animateLogo();
   }
 
   ngAfterViewInit() {
-
+    this.showContainer();
   }
 
   showContainer(){
@@ -108,118 +107,8 @@ export class AppComponent implements AfterViewInit {
     this.sidebarService.toggleSidebarStatus();
   }
 
-  //define center of the logo explosion
-  getCenter(bounds){
-    let centerY = window.innerHeight / 2;
-    let centerX = bounds.x + bounds.width /2;
-
-    return {x: centerX, y: centerY};
-  }
 
 
-  animateLogo(){
-    console.log('animate logo');
-    this._animating = true;
-
-    const stagger = 5;
-
-    this.el = this.elementRef.nativeElement;
-    TweenLite.defaultEase = Linear.easeNone;
-    let logoElement = this.el.querySelector('#logo-ng-rome');
-    let bounds = logoElement.getBBox();
-    let blast = this.el.querySelector('#blast');
-    let center = this.getCenter(bounds);
-    //check the window height
-
-    var radius = this.getDistance(center, bounds);
-
-    //TweenMax.set(blast, { attr: { r: radius, cx: center.x, cy: center.y }});
-    TweenLite.set(blast, { transformOrigin: "center", scale: 0 });
-
-
-    //tl.to(blast, stagger, { scale: 1 }, 0);
-    this._tl.to(blast, stagger, { scale: 0, autoAlpha: 1 }, stagger);
-
-    let logoElementsToAnimate = Array.prototype.slice.call( logoElement.children );
-    //console.log('1-get element to animate ');
-    logoElementsToAnimate.forEach( (element,i) => {
-      //console.log('3-forEach element ',i);
-      var bbox = element.getBBox();
-      var scale = 1;
-      var toBlur = false;
-
-      //define the element to blur
-      if (i%2==0 && element.className.baseVal === 'toBlur'){
-        scale = Math.random() * 2 + 1;
-        if (scale > 1.7){
-          toBlur = true;
-          //console.log('4-blur this element ',element);
-        }
-      }
-
-      //this item should be not visible
-      if (element.className.baseVal === 'long'){
-        scale = 0;
-      }
-      var dist = this.getDistance(bbox, center);
-      var delay = 0.1;
-
-      var scalar = radius / dist;
-
-      //console.log('6-scalar ', scalar);
-      var itemRotation = Math.floor(Math.random() * (365 - 90) + 90);
-      //console.log('7-rotate this element ', itemRotation);
-      var rotation = itemRotation+"_short";
-
-      this._tl.to(element, 3, {
-        autoAlpha: 1,
-        x: (bbox.x - center.x) * scalar,
-        y: (bbox.y - center.y) * scalar,
-        directionalRotation:rotation,
-        scale: scale
-      }, delay);
-
-      if(toBlur){
-
-        setTimeout(() => {
-          //console.log('show container');
-          element.classList.add('blur');
-        }, 2000);
-
-      }
-    });
-
-    let animation = this.isActive ? this.inactivate() : this.activate();
-    this._active = !this.isActive;
-
-    //return new TimelineLite().add(animation);
-    this.showContainer();
-
-  }
-
-  getDistance(p1, p2) {
-    var dx = p2.x - p1.x;
-    var dy = p2.y - p1.y;
-    return Math.sqrt(dx * dx + dy * dy);
-  }
-
-  activate(){
-    console.log('activate');
-    return TweenLite.to(this._tl, 5, {
-      progress: 10,
-      ease: Expo.easeInOut,
-      repeat: 0,
-    },{ className: "" }, { className: "active" });
-  }
-
-  inactivate(){
-    console.log('inactivate');
-    return TweenLite.to(this._tl, 8, {
-      progress: 10,
-      ease: Expo.easeInOut,
-      repeat: 0,
-    },{ className: "active" }, { className: "" });
-  }
 
   // resetAnimationLogo(){
   //   let logoElement = this.el.querySelector('#logo-ng-rome');
