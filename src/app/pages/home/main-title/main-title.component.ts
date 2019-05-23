71e6160bb8a537e65f128ad4cc7bb29309c046e8
Luciano Murruni <luciano.murruni@conte.it>
2019-05-23 10:47:44 +0200

    Fix title mq issue

--- src/app/pages/home/main-title/main-title.component.ts
+++ src/app/pages/home/main-title/main-title.component.ts
@@ -11,44 +11,46 @@ import { NgAnimateScrollService } from 'src/app/shared/services/ng-animate-scrol
         <div class="site-content__intro">
           <header class="site-content__intro__head">
             <h1 class="site-content__intro__title">
-              For the first time ever, the biggest <strong>Italian Angular Conference</strong> 
-              is taking place at <strong>NG Rome MMXIX</strong> 
+              For the first time ever, the biggest <strong>Italian Angular Conference</strong>
+              is taking place at <strong>NG Rome MMXIX</strong>
               on <strong>October 7th</strong> in <strong>Rome, Italy.</strong>
             </h1>
             <div class="site-content__intro__description">
-              <a class="button" id="go-to-tickets" (click)="scrollTo('tickets', 300)" 
+              <a class="button" id="go-to-tickets" (click)="scrollTo('tickets', 300)"
               title="Get Ticket Now">
                 <span id="go-to-tickets-text">Get Ticket Now</span>
                 <svg width="11" height="12" id="go-to-tickets-svg">
-                  <path fill="none" fill-rule="evenodd" stroke="#0086FF" 
+                  <path fill="none" fill-rule="evenodd" stroke="#0086FF"
                   stroke-width="2" d="M1 1l7.358 5L1 11"/>
                 </svg>
-              </a>
-              <a class="button"
-              href="/speakers"
-              routerLinkActive="router-link-active"
-              title="NGRome Speakers">
-                <span id="go-to-tickets-text">Speakers</span>  
-                <svg width="11" height="12" id="go-to-tickets-svg">
-                  <path fill="none" fill-rule="evenodd" stroke="#0086FF" 
-                  stroke-width="2" d="M1 1l7.358 5L1 11"/>
-                </svg> 
               </a>
             </div>
             <span class="site-content__intro__scroller"></span>
           </header>
           <div class="site-content__intro__info">
-            <p>We will have <b>15 talks</b> about the latest version of 
+            <p>We will have <b>15 talks</b> about the latest version of
             Angular.</p>
-            <p>Whether you are a beginner, intermediate, or an expert, 
-            this will be the best opportunity to learn about the last 
+            <p>Whether you are a beginner, intermediate, or an expert,
+            this will be the best opportunity to learn about the last
             trends in the <b>Angular</b> world and <b>Web Technologies</b>.</p>
-            <p>We will cover <b>topics</b> like: Blockchain, GraphQL, Machine Learning, NgRx, 
-            New Angular features, Progressive Web Apps, RxJs, Testing, Web Performance, 
+            <p>We will cover <b>topics</b> like: Blockchain, GraphQL, Machine Learning, NgRx,
+            New Angular features, Progressive Web Apps, RxJs, Testing, Web Performance,
             Ionic, How to learn Javascript, etc.</p>
-            <p>A <b>full day</b> of news and tips about Angular and the possibility 
+            <p>A <b>full day</b> of news and tips about Angular and the possibility
             to share ideas and meet top <b>Italian</b> and International developers.</p>
           </div>
+          <div class="site-content__intro__description">
+              <a class="button"
+              href="/speakers"
+              routerLinkActive="router-link-active"
+              title="NGRome Speakers">
+                <span id="go-to-tickets-text">Speakers</span>
+                <svg width="11" height="12" id="go-to-tickets-svg">
+                  <path fill="none" fill-rule="evenodd" stroke="#0086FF"
+                  stroke-width="2" d="M1 1l7.358 5L1 11"/>
+                </svg>
+              </a>
+            </div>
         </div>
       </div>
     </section>
