c7a9d566646738a150d180d0acf9e8921eb5f83d
LordKenzo <lordkenzo@outlook.it>
2019-06-19 13:11:41 +0200

    Some fix on subscribe, and delete next and prev property to suggestion-speaker component

--- src/app/pages/speakers/speaker-detail/speaker-detail.component.ts
+++ src/app/pages/speakers/speaker-detail/speaker-detail.component.ts
@@ -2,43 +2,41 @@ import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@
 import { ActivatedRoute, Router, Event, NavigationStart, NavigationEnd } from '@angular/router';
 import { Observable, Subscription } from 'rxjs';
 import { AngularFirestore } from '@angular/fire/firestore';
-
-
+import { reduce, tap, filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'ngrome-speaker-detail',
   templateUrl: './speaker-detail.component.html',
   styleUrls: ['./speaker-detail.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SpeakerDetailComponent implements OnInit {
-
   private _paramSpeakerName;
-  private _routerSubscribe: Subscription;
-  private _speakerSubscribe: Subscription;
+  // private _routerSubscribe: Subscription;
+  // private _speakerSubscribe: Subscription;
 
-  public previous = 0;
-  public next = 0;
+  // public previous = 0;
+  // public next = 0;
 
-  speaker$: Observable<any[]>;
+  speaker$: Observable<any>;
 
   constructor(
     private _Activatedroute: ActivatedRoute,
     private _router: Router,
     private afs: AngularFirestore,
-    private cd: ChangeDetectorRef) {
-
-  }
+    private cd: ChangeDetectorRef,
+  ) {}
 
   ngOnChanges() {
     console.log('something changes');
   }
 
-
-
   ngOnInit() {
+    this._Activatedroute.params.subscribe((speaker: { name: string }) =>
+      this.getSpeakerDetail(speaker),
+    );
 
-    this._paramSpeakerName = this._Activatedroute.snapshot.params['name'];
+    /*this._paramSpeakerName = this._Activatedroute.snapshot.params['name'];
     this.getSpeakerDetail();
 
     this._routerSubscribe = this._router.events.subscribe( (event:Event) => {
@@ -47,34 +45,40 @@ export class SpeakerDetailComponent implements OnInit {
         this._paramSpeakerName = this._Activatedroute.snapshot.params['name'];
         this.getSpeakerDetail();
       }
-    });
-
+    });*/
   }
 
+  getSpeakerDetail({ name }) {
+    console.log('getSpeakerDetail', name);
+    this.speaker$ = this.afs
+      .collection('speakers', ref => {
+        let query: firebase.firestore.CollectionReference | firebase.firestore.Query = ref;
+        query = query.where('name', '==', name);
+        query = query.orderBy('position', 'asc');
+        // console.log(query);
+        return query;
+      })
+      .valueChanges()
+      .pipe(
+        map(x => {
+          console.log(x);
+          return x.reduce((acc, curr) => {
+            return curr;
+          }, {});
+        }),
+        tap(y => console.log(y)),
+      );
 
-  getSpeakerDetail(){
-    console.log('getSpeakerDetail', this._paramSpeakerName);
-    this.speaker$ =
-      this.afs.collection('speakers',
-        ref => {
-          let query: firebase.firestore.CollectionReference | firebase.firestore.Query = ref;
-          query = query.where('name', '==', this._paramSpeakerName);
-          query = query.orderBy('position', 'asc');
-          // console.log(query);
-          return query;
-        }).valueChanges();
-
-      this._speakerSubscribe = this.speaker$.subscribe((result) => {
-        this.previous = result[0].position - 1;
-        this.next = result[0].position + 1;
-        console.log('detail: ', this.previous, this.next)
-      });
+    /*this._speakerSubscribe = this.speaker$.subscribe(result => {
+      this.previous = result[0].position - 1;
+      this.next = result[0].position + 1;
+      console.log('detail: ', this.previous, this.next);
+    });*/
   }
 
-  ngOnDestroy(){
+  ngOnDestroy() {
     console.log('on destroy');
-    this._routerSubscribe.unsubscribe();
-    this._speakerSubscribe.unsubscribe();
+    // this._routerSubscribe.unsubscribe();
+    // this._speakerSubscribe.unsubscribe();
   }
-
 }
--- src/app/pages/speakers/speaker-suggestion/speaker-suggestion.component.ts
+++ src/app/pages/speakers/speaker-suggestion/speaker-suggestion.component.ts
@@ -5,15 +5,10 @@ import { AngularFirestore } from '@angular/fire/firestore';
 @Component({
   selector: 'ngrome-speaker-suggestion',
   templateUrl: './speaker-suggestion.component.html',
-  styleUrls: ['./speaker-suggestion.component.scss']
+  styleUrls: ['./speaker-suggestion.component.scss'],
 })
 export class SpeakerSuggestionComponent implements OnInit {
-
-  @Input()
-  previous
-
-  @Input()
-  next;
+  @Input() current: number;
 
   nextSpeaker$: Observable<any[]>;
   previousSpeaker$: Observable<any[]>;
@@ -21,38 +16,33 @@ export class SpeakerSuggestionComponent implements OnInit {
   public emptyPrevious = false;
   public emptyNext = false;
 
-  constructor(
-    private afs: AngularFirestore
-  ) {
-    if (this.previous === 0){
+  constructor(private afs: AngularFirestore) {
+    if (this.current === 0) {
       this.emptyPrevious = true;
     }
   }
 
   ngOnInit() {
-    console.log('suggestion:',this.previous, this.next);
+    console.log('suggestion:', this.current);
 
-    if (this.previous != 0){
-      this.previousSpeaker$ =
-      this.afs.collection('speakers',
-          ref => {
-            let query : firebase.firestore.CollectionReference | firebase.firestore.Query = ref;
-            query = query.where('position', '==', this.previous);
-            return query;
-          }).valueChanges();
+    if (this.current !== 0) {
+      this.previousSpeaker$ = this.afs
+        .collection('speakers', ref => {
+          let query: firebase.firestore.CollectionReference | firebase.firestore.Query = ref;
+          query = query.where('position', '==', this.current - 1);
+          return query;
+        })
+        .valueChanges();
     }
 
-
-    if (this.next <= 16){
-      this.nextSpeaker$ =
-        this.afs.collection('speakers',
-            ref => {
-              let query : firebase.firestore.CollectionReference | firebase.firestore.Query = ref;
-              query = query.where('position', '==', this.next);
-              return query;
-            }).valueChanges();
+    if (this.current <= 16) {
+      this.nextSpeaker$ = this.afs
+        .collection('speakers', ref => {
+          let query: firebase.firestore.CollectionReference | firebase.firestore.Query = ref;
+          query = query.where('position', '==', this.current + 1);
+          return query;
+        })
+        .valueChanges();
     }
-
   }
-
 }
