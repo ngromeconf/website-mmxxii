7c5920a304a7496000095d89381700fad4f43542
Luciano Murruni <luciano.murruni@conte.it>
2019-06-23 17:45:21 +0200

    add the info page

--- src/app/pages/info/info.component.ts
+++ src/app/pages/info/info.component.ts
@@ -12,4 +12,8 @@ export class InfoComponent implements OnInit {
   ngOnInit() {
   }
 
+  addToCalendar(){
+    console.log('event added to calendar');
+  }
+
 }
--- src/app/pages/info/info.module.ts
+++ src/app/pages/info/info.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { InfoComponent } from './info.component';
 import { RouterModule } from '@angular/router';
 
+import { SharedModule } from 'src/app/shared/shared.module';
+
 @NgModule({
   declarations: [InfoComponent],
   imports: [
@@ -10,6 +12,7 @@ import { RouterModule } from '@angular/router';
     RouterModule.forChild([
       {path: '', component: InfoComponent, pathMatch: 'full'}
     ]),
+    SharedModule
   ]
 })
 export class InfoModule { }
