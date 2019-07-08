909c09ed0c6ed9e80857b65a366bc7001bd6e0f8
Luciano Murruni <luciano.murruni@conte.it>
2019-07-08 09:53:03 +0200

    Fix some build --prod error

--- src/app/pages/workshop/workshop.module.ts
+++ src/app/pages/workshop/workshop.module.ts
@@ -3,16 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { WorkshopComponent } from './workshop.component';
-import { WorkshopDetailComponent } from './workshop-detail/workshop-detail.component';
 import { TicketsComponent } from './tickets/tickets.component';
 
 @NgModule({
-  declarations: [WorkshopComponent, WorkshopDetailComponent, TicketsComponent],
+  declarations: [WorkshopComponent, TicketsComponent],
   imports: [
     CommonModule,
     RouterModule.forChild([
       { path: '', component: WorkshopComponent, pathMatch: 'full' },
-      { path: 'workshop-detail/:name', component: WorkshopDetailComponent },
     ]),
     SharedModule,
   ],
