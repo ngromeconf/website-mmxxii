8ade3403e0bb33e7614b916aef2c5db20d5b9e01
Leonardo Zizzamia <leonardo.zizzamia@coinbase.com>
2019-07-10 15:24:55 -0700

    Updated the About page

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,15 +1,88 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'ngrome-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  template: `
+  <section id="intro" class="site-content__section">
+    <div class="site-content__wrap">
+      <div class="site-content__intro">
+        <header class="site-content__intro__head__inner">
+          <h1 class="site-content__intro__title">About</h1>
+        </header>
+        <div class="site-content__intro__info__inner">
+          <p>NG-Rome is a <b>non-profit community</b> conference run by a team of volunteers.
+          We are all active members of the tech community, and run or contribute to various free 
+          local meetups, workshops, and education initiatives.</p>
+          <p>NG-Rome event is the biggest <b>International</b> Angular Conference in Italy,
+          helping bring together ideas and developers from all the <b>corners of Italy</b> and Europe.</p>
+          <p>The community is born by a group of passionate devs, JavaScript lovers,
+          and enthusiastic developer that use the <b>Angular</b> framework.</p>
+          <p>Our mission is to connect Italian developers to the <b>global community</b>.</p>
+          <p>Fostering new relationships within Italy, and across borders.
+          Simultaneously develop new skills and having <b>moments of fun</b>.</p>
+          <div class="content__intro__info__inner__yolo">
+            <blockquote class="twitter-tweet" data-lang="en">
+              <p lang="en" dir="ltr">A taste of 
+              <a href="https://twitter.com/hashtag/NGRomeMMXIX?src=hash&amp;ref_src=twsrc%5Etfw">#NGRomeMMXIX</a> 🇮🇹 
+              <a href="https://t.co/9gaoN3AJer">pic.twitter.com/9gaoN3AJer</a></p>&mdash; NGRomeConf (@ngromeconf) 
+              <a href="https://twitter.com/ngromeconf/status/1131316168487317505?ref_src=twsrc%5Etfw">May 22, 2019</a>
+            </blockquote>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="site-content__wrap"></div>
+  </section>
+  <!-- section class="site-content__section">
+    <div class="site-content__wrap">
+      <div class="site-content__description">
+        <h1>The Team</h1>
+      </div>
+    </div>
+  </section -->
+  <ngrome-tickets class="site-content__section" id="tickets"></ngrome-tickets>
+  `,
+  styles: [`
+    .site-content {
+      &__wrap{
+        img{
+          width: 100%;
+        }
+      }
+      &__description{
+        max-width: 700px;
+        margin: auto;
+        h1 {
+          font-size: 24px;
+          font-weight: 700;
+          line-height: 1.1;
+        }
+        @media only screen and (min-width: 768px) {
+          h1 {
+            font-size: 32px;
+            flex-basis: 40%;
+            padding-right: 80px;
+          }
+        }
+      }
+    }
+    .content__intro__info__inner__yolo {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+  `]
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private elementRef: ElementRef) { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    const s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.src = 'https://platform.twitter.com/widgets.js';
+    this.elementRef.nativeElement.appendChild(s);
   }
-
 }
