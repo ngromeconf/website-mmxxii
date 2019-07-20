da4f72333f5428069b4947c39ec8ec14d8bfadae
Luciano Murruni <luciano.murruni@conte.it>
2019-07-20 22:43:35 +0200

    FIX: remove unused ElementRef import

--- src/app/shared/speakers-list/speakers-list.component.ts
+++ src/app/shared/speakers-list/speakers-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable, BehaviorSubject, combineLatest } from 'rxjs';
