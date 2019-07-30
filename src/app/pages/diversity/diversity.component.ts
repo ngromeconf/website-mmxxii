61f23a608b3b458d280bcc46e6cf4ae491222bc4
Luciano Murruni <luciano.murruni@conte.it>
2019-07-30 08:37:45 +0200

    Add diversity page

---
+++ src/app/pages/diversity/diversity.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngrome-diversity',
+  template: `
+    <p>
+      diversity works!
+    </p>
+  `,
+  styles: []
+})
+export class DiversityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/pages/diversity/diversity.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DiversityComponent } from './diversity.component';
+
+@NgModule({
+  declarations: [DiversityComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class DiversityModule { }
