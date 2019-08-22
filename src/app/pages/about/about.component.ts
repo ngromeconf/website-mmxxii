2b68cd60b1ad46c946251ea9ebd81525663b91af
Luciano Murruni <luciano.murruni@conte.it>
2019-08-22 16:02:54 +0200

    add the team into about page

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -11,7 +11,7 @@ import { Component, OnInit, ElementRef } from '@angular/core';
         </header>
         <div class="site-content__intro__info__inner">
           <p>NG-Rome is a <b>non-profit community</b> conference run by a team of volunteers.
-          We are all active members of the tech community, and run or contribute to various free 
+          We are all active members of the tech community, and run or contribute to various free
           local meetups, workshops, and education initiatives.</p>
           <p>NG-Rome event is the biggest <b>International</b> Angular Conference in Italy,
           helping bring together ideas and developers from all the <b>corners of Italy</b> and Europe.</p>
@@ -22,9 +22,9 @@ import { Component, OnInit, ElementRef } from '@angular/core';
           Simultaneously develop new skills and having <b>moments of fun</b>.</p>
           <div class="content__intro__info__inner__yolo">
             <blockquote class="twitter-tweet" data-lang="en">
-              <p lang="en" dir="ltr">A taste of 
-              <a href="https://twitter.com/hashtag/NGRomeMMXIX?src=hash&amp;ref_src=twsrc%5Etfw">#NGRomeMMXIX</a> 🇮🇹 
-              <a href="https://t.co/9gaoN3AJer">pic.twitter.com/9gaoN3AJer</a></p>&mdash; NGRomeConf (@ngromeconf) 
+              <p lang="en" dir="ltr">A taste of
+              <a href="https://twitter.com/hashtag/NGRomeMMXIX?src=hash&amp;ref_src=twsrc%5Etfw">#NGRomeMMXIX</a> 🇮🇹
+              <a href="https://t.co/9gaoN3AJer">pic.twitter.com/9gaoN3AJer</a></p>&mdash; NGRomeConf (@ngromeconf)
               <a href="https://twitter.com/ngromeconf/status/1131316168487317505?ref_src=twsrc%5Etfw">May 22, 2019</a>
             </blockquote>
           </div>
@@ -33,13 +33,50 @@ import { Component, OnInit, ElementRef } from '@angular/core';
     </div>
     <div class="site-content__wrap"></div>
   </section>
-  <!-- section class="site-content__section">
+
+
+  <section class="site-content__section">
     <div class="site-content__wrap">
       <div class="site-content__description">
-        <h1>The Team</h1>
+        <h1 class="site-content__section__title">The TEAM</h1>
+        <ul class="team__list">
+          <li>
+            <img src="./assets/images/team/zizzamia.png"><br>
+            <span>Leonardo Zizzamia</span>
+          </li>
+          <li>
+            <img src="./assets/images/team/kraus.png"><br>
+            <span>Martina Kraus</span>
+          </li>
+          <li>
+            <img src="./assets/images/team/murruni.png"><br>
+            <span>Luciano Murruni</span>
+          </li>
+          <li>
+            <img src="./assets/images/team/bertaina.png"><br>
+            <span>Michela Bertaina</span>
+          </li>
+          <li>
+            <img src="./assets/images/team/carducci.png"><br>
+            <span>Luca Carducci</span>
+          </li>
+          <li>
+            <img src="./assets/images/team/franceschini.png"><br>
+            <span>Lorenzo Franceschini</span>
+          </li>
+          <li>
+            <img src="./assets/images/team/matascioli.png"><br>
+            <span>Marco Matascioli</span>
+          </li>
+          <li>
+            <img src="./assets/images/team/natili.png"><br>
+            <span>Giorgio Natili</span>
+          </li>
+        </ul>
       </div>
     </div>
-  </section -->
+  </section>
+
   <ngrome-tickets class="site-content__section" id="tickets"></ngrome-tickets>
   `,
   styles: [`
