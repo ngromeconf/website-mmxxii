ec08f190db533beef10d6e4fba3d857f1b4f8386
Ermanno <ermanno.battista@gmail.com>
2020-02-13 17:35:15 +0100

    Add intro cover

--- src/app/pages/home/past-edition-video/past-edition-video.component.ts
+++ src/app/pages/home/past-edition-video/past-edition-video.component.ts
@@ -4,37 +4,8 @@ import {NgAnimateScrollService} from '../../../shared/services/ng-animate-scroll
 
 @Component({
   selector: 'ngrome-past-edition-video',
-  styles: [],
-  template: `
-    <div class="site-content__wrap">
-      <div class="site-content__intro">
-        <ngrome-youtube-player-with-covers videoId="8XqmTwuxGd0">
-          <div intro-cover (click)="play()"><img src="https://via.placeholder.com/1000x396/EE0C3E/FFFFFF?text=Click me to start" width="100%"></div>
-          <div end-cover>
-            <p>
-            <a class="button" id="go-to-tickets" (click)="scrollTo('tickets', 300)"
-               title="Get Ticket Now">
-              <span id="go-to-tickets-text-video">Get Ticket Now</span>
-              <svg width="11" height="12" id="go-to-tickets-svg">
-                <path fill="none" fill-rule="evenodd" stroke="#0086FF"
-                      stroke-width="2" d="M1 1l7.358 5L1 11"/>
-              </svg>
-            </a>
-            </p>
-            <p>
-            <a class="button" (click)="play()"
-               title="Replay Video">
-              <span id="go-to-tickets-text">Replay Video</span>
-              <svg width="11" height="12" id="go-to-tickets-svg">
-                <path fill="none" fill-rule="evenodd" stroke="#EE0C3E"
-                      stroke-width="2" d="M1 1l7.358 5L1 11"/>
-              </svg>
-            </a>
-            </p>
-          </div>
-        </ngrome-youtube-player-with-covers>
-      </div>
-    </div>`
+  styleUrls: ['past-edition-video.component.scss'],
+  templateUrl: 'past-edition-video.component.html'
 })
 export class PastEditionVideoComponent implements OnInit {
   @ViewChild(YoutubePlayerWithCoversComponent, {static: true}) player;
