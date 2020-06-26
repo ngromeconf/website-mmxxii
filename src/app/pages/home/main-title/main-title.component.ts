a798c1d569dd0b8a73e2a59d805326a99af476e6
Luciano Murruni <luciano.murruni@conte.it>
2020-06-26 14:18:12 +0200

    Some fix on main layout

--- src/app/pages/home/main-title/main-title.component.ts
+++ src/app/pages/home/main-title/main-title.component.ts
@@ -15,27 +15,40 @@ import { NgAnimateScrollService } from "src/app/shared/services/ng-animate-scrol
             </h1>
             <img src="assets/logo/logo-mmxx-red.svg" />
             <div class="site-content__intro__description">
+              <div>
+                <span>where</span>
+                <strong>Rome, Italy</strong>
+              </div>
+              <div>
+                <span>when</span>
+                <strong>October 24th 2020</strong>
+              </div>
               <a
-                class="button"
-                id="go-to-tickets"
+                class="button button--green button--fill-green"
+                rel="noopener"
+                target="_blank"
                 (click)="scrollTo('tickets', 300)"
-                title="Get Ticket Now"
-              >
-                <span id="go-to-tickets-text">Get Ticket Now</span>
-                <svg width="11" height="12" id="go-to-tickets-svg">
+                title="Get ticket now"
+                >Get tickets
+                <svg
+                  width="14"
+                  height="17"
+                  viewBox="0 0 14 17"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
                   <path
-                    fill="none"
                     fill-rule="evenodd"
-                    stroke="#0086FF"
-                    stroke-width="2"
-                    d="M1 1l7.358 5L1 11"
+                    clip-rule="evenodd"
+                    d="M11.5362 2.75626C10.9485 3.5487 9.85762 3.8427 8.91969 3.40535C7.98021 2.96728 7.50422 1.94268 7.735 0.983813L6.11255 0.22728L0.272911 12.7509L1.85519 13.4887C2.34835 12.4311 3.61668 11.9783 4.6875 12.4776C5.75832 12.9769 6.22678 14.2395 5.73362 15.2972L7.3159 16.035L13.1555 3.51136L11.5362 2.75626ZM7.18465 11.3381L3.95674 9.83297L6.24534 4.92486L9.47325 6.43L7.18465 11.3381Z"
+                    fill="#F9F9F9"
                   />
                 </svg>
               </a>
             </div>
-            <span class="site-content__intro__scroller"></span>
+            <!-- <span class="site-content__intro__scroller"></span> -->
           </header>
-          <div class="site-content__intro__info">
+          <!-- <div class="site-content__intro__info">
             <p>
               Whether you are a beginner, intermediate, or an expert, this will
               be the best opportunity to learn about the <b>last trends</b> in
@@ -43,36 +56,31 @@ import { NgAnimateScrollService } from "src/app/shared/services/ng-animate-scrol
               tips about <b>Angular</b> and the possibility to share ideas and
               meet top <b>Italian and International developers</b>.
             </p>
-          </div>
+          </div> -->
           <!-- <div class="site-content__intro__description">
-              <a class="button"
+            <a
+              class="button"
               routerLink="/speakers"
               routerLinkActive="router-link-active"
-              title="NGRome Speakers">
-                <span id="go-to-tickets-text">Speakers</span>
-                <svg width="11" height="12" id="go-to-tickets-svg">
-                  <path fill="none" fill-rule="evenodd" stroke="#0086FF"
-                  stroke-width="2" d="M1 1l7.358 5L1 11"/>
-                </svg>
-              </a>
-            </div> -->
+              title="NGRome Speakers"
+            >
+              <span id="go-to-tickets-text">Speakers</span>
+              <svg width="11" height="12" id="go-to-tickets-svg">
+                <path
+                  fill="none"
+                  fill-rule="evenodd"
+                  stroke="#0086FF"
+                  stroke-width="2"
+                  d="M1 1l7.358 5L1 11"
+                />
+              </svg>
+            </a>
+          </div> -->
         </div>
       </div>
     </section>
   `,
-  styles: [
-    `
-      .site-content__intro__description {
-        display: flex;
-      }
-      .site-content__intro__description a {
-        margin-right: 30px;
-      }
-      .site-content__intro__info b {
-        font-weight: bolder;
-      }
-    `,
-  ],
+  styles: [``],
 })
 export class MainTitleComponent {
   actualPage: string;
--- src/app/shared/youtube-player/youtube-player-with-covers/youtube-player-with-covers.component.ts
+++ src/app/shared/youtube-player/youtube-player-with-covers/youtube-player-with-covers.component.ts
@@ -1,12 +1,28 @@
-import {AfterContentInit, AfterViewInit, Component, ElementRef, EventEmitter, HostListener, Input, OnInit, ViewChild} from '@angular/core';
-import {debounceTime} from 'rxjs/operators';
-import {animate, state, style, transition, trigger} from '@angular/animations';
-import {YouTubePlayer} from '@angular/youtube-player';
+import {
+  AfterContentInit,
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  HostListener,
+  Input,
+  OnInit,
+  ViewChild,
+} from "@angular/core";
+import { debounceTime } from "rxjs/operators";
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from "@angular/animations";
+import { YouTubePlayer } from "@angular/youtube-player";
 
 @Component({
-  selector: 'ngrome-youtube-player-with-covers',
-  templateUrl: './youtube-player-with-covers.component.html',
-  styleUrls: ['./youtube-player-with-covers.component.scss']
+  selector: "ngrome-youtube-player-with-covers",
+  templateUrl: "./youtube-player-with-covers.component.html",
+  styleUrls: ["./youtube-player-with-covers.component.scss"],
 })
 export class YoutubePlayerWithCoversComponent implements OnInit, AfterViewInit {
   @Input() videoId: string;
@@ -15,10 +31,10 @@ export class YoutubePlayerWithCoversComponent implements OnInit, AfterViewInit {
   playCount = 0;
   playingChange$ = new EventEmitter<boolean>();
 
-  @ViewChild('container', {static: true}) container: ElementRef;
+  @ViewChild("container", { static: true }) container: ElementRef;
   fixedHeight: number;
   fixedWidth: number;
-  constructor() { }
+  constructor() {}
 
   ngAfterViewInit(): void {
     this.computePlayerDimensions();
@@ -28,17 +44,17 @@ export class YoutubePlayerWithCoversComponent implements OnInit, AfterViewInit {
     // From: https://github.com/angular/components/blob/8.2.3/src/youtube-player/README.md
     // This code loads the IFrame Player API code asynchronously, according to the instructions at
     // https://developers.google.com/youtube/iframe_api_reference#Getting_Started
-    const tag = document.createElement('script');
+    const tag = document.createElement("script");
 
-    tag.src = 'https://www.youtube.com/iframe_api';
+    tag.src = "//www.youtube.com/iframe_api";
     document.body.appendChild(tag);
 
     this.playingChange$
       .pipe(debounceTime(100))
-      .subscribe((playing) => this.playing = playing);
+      .subscribe((playing) => (this.playing = playing));
   }
 
-  @HostListener('window:resize', ['$event'])
+  @HostListener("window:resize", ["$event"])
   onResize(event) {
     this.computePlayerDimensions();
   }
@@ -46,29 +62,34 @@ export class YoutubePlayerWithCoversComponent implements OnInit, AfterViewInit {
   private computePlayerDimensions() {
     setTimeout(() => {
       this.fixedWidth = this.container.nativeElement.offsetWidth;
-      this.fixedHeight = Math.trunc(this.fixedWidth * 9 / 16); /* 16:9 Aspect Ratio */
+      this.fixedHeight = Math.trunc(
+        (this.fixedWidth * 9) / 16
+      ); /* 16:9 Aspect Ratio */
     }, 1);
   }
 
   onReady($evt: YT.PlayerEvent) {
-    console.log('player-ready', $evt);
+    console.log("player-ready", $evt);
     this.playerRef = $evt.target;
   }
 
   onStateChange($evt: YT.OnStateChangeEvent) {
-    console.log('player-state-change', $evt);
-    this.playingChange$.emit(($evt.data === YT.PlayerState.PLAYING || $evt.data === YT.PlayerState.BUFFERING));
+    console.log("player-state-change", $evt);
+    this.playingChange$.emit(
+      $evt.data === YT.PlayerState.PLAYING ||
+        $evt.data === YT.PlayerState.BUFFERING
+    );
 
     switch ($evt.data) {
       case YT.PlayerState.PLAYING:
         this.playCount++;
-        console.log('youtube playing');
+        console.log("youtube playing");
         break;
       case YT.PlayerState.PAUSED:
-        console.log('youtube paused');
+        console.log("youtube paused");
         break;
       case YT.PlayerState.ENDED:
-        console.log('youtube stop');
+        console.log("youtube stop");
         // Stop the video on ending so recommended videos don't pop up
         this.playerRef.stopVideo();
         break;
