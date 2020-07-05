13f5348e43067dfd5263ebf7da0e2b9db022bdf6
cesare <laurenziello@hotmail.it>
2020-07-06 01:53:01 +0200

    add _about.scss

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -4,11 +4,11 @@ import { SeoService } from 'src/app/shared/services/seo.service';
 @Component({
   selector: 'ngrome-about',
   template: `
-    <div class="flex-container">
-      <header>
-        <h1 class="about-title">About</h1>
+    <div class="site-about">
+      <header class="site-about__header">
+        <h1>About</h1>
       </header>
-      <div class="about-text">
+      <div class="about-text site-about__main">
         <p>
           NG-Rome is a <b>non-profit community</b> conference run by a team of
           volunteers. We are all active members of the tech community, and run
@@ -34,7 +34,7 @@ import { SeoService } from 'src/app/shared/services/seo.service';
           <b>moments of fun</b>.
         </p>
       </div>
-      <div class="about-twitter">
+      <div class="site-about__twitter">
         <blockquote class="twitter-tweet" data-lang="en">
           <p lang="en" dir="ltr">
             A taste of
@@ -51,11 +51,11 @@ import { SeoService } from 'src/app/shared/services/seo.service';
             >May 22, 2019</a
           >
         </blockquote>
-        <div class="about-twitter-hide">
+        <div class="site-about__twitter__text">
           STAY INFORMED
         </div>
       </div>
-      <div class="about-team">
+      <div class="site-about__team">
         <h1>The TEAM</h1>
         <div>
           <div>
@@ -128,120 +128,6 @@ import { SeoService } from 'src/app/shared/services/seo.service';
 
     <!-- <ngrome-tickets class="site-content__section" id="tickets"></ngrome-tickets> -->
   `,
-  styles: [
-    `
-      .flex-container {
-        margin-top: 60px;
-        display: flex;
-        flex-direction: column;
-      }
-
-      @media only screen and (min-width: 768px) {
-        .flex-container {
-          padding-left: 100px;
-          padding-top: 100px;
-        }
-      }
-
-      .about-title {
-        font-weight: bold;
-        font-size: 80px;
-        letter-spacing: 0.07em;
-        text-transform: uppercase;
-        color: #de0031;
-        padding: 20px;
-      }
-      .about-text {
-        background-image: url('/assets/images/about-bckg.jpg');
-        background-repeat: no-repeat;
-        background-position: bottom right;
-        height: 150%;
-      }
-      .about-text > p {
-        padding: 20px;
-        font-style: normal;
-        font-weight: normal;
-        font-size: 25px;
-        line-height: 148.6%;
-        /* or 53px */
-        letter-spacing: -0.02em;
-      }
-
-      @media only screen and (min-width: 768px) {
-        .about-text > p {
-          margin-right: 250px;
-        }
-      }
-      .about-text > p:first-child {
-        font-size: 36px;
-        color: #dd0031;
-      }
-      .about-text > p:last-child {
-        margin-bottom: 200px;
-      }
-      .about-twitter {
-        display: flex;
-        justify-content: center;
-      }
-
-      .about-twitter > div {
-        padding-left: 50px;
-        padding-top: 150px;
-        font-style: normal;
-        font-weight: bold;
-        font-size: 80px;
-        line-height: 118.6%;
-        /* or 90px */
-
-        letter-spacing: 0.07em;
-        text-transform: uppercase;
-        color: #dd0031;
-      }
-
-      @media only screen and (max-width: 1224px) {
-        .about-twitter > div {
-          display: none;
-        }
-      }
-
-      .about-team {
-        display: flex;
-        flex-direction: column;
-        padding-top: 50px;
-      }
-
-      .about-team h1 {
-        font-weight: bold;
-        font-size: 37px;
-        line-height: 148.6%;
-        letter-spacing: 0.07em;
-        text-transform: uppercase;
-        color: #dd0031;
-        text-align: center;
-      }
-      .about-team > div {
-        display: flex;
-        justify-content: center;
-        flex-wrap: wrap;
-      }
-
-      .about-team > div > div {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        padding: 40px;
-      }
-
-      .about-team > div > div > a {
-        text-align: center;
-        padding-top: 20px;
-      }
-
-      .about-team img {
-        border-radius: 50%;
-      }
-    `,
-  ],
 })
 export class AboutComponent implements OnInit {
   constructor(private elementRef: ElementRef, private seoService: SeoService) {}
