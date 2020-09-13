1656254ac9da1649acdd9937f12d24a00944880a
RJ Burgos <ryanjpburgos@gmail.com>
2020-09-13 20:25:59 +0200

    new tickets section

--- src/app/shared/tickets/tickets.component.ts
+++ src/app/shared/tickets/tickets.component.ts
@@ -4,29 +4,31 @@ import { MessagingService } from 'src/app/shared/services/messaging.service';
 @Component({
   selector: 'ngrome-tickets',
   template: `
-  <section #tickets class="site-content__section">
-    <div class="site-content__wrap">
-      <h1 class="site-content__section__title">Buy Ticket</h1>
-      <div class="tickets__list">
-        <article class="ticket ticket--ready">
-          <a href="https://ti.to/ngrome-conf/NGRome-Conf-MMXX-Online/with/ujp-ev2-cqs"
-            title="Ticket">
-            <h2 class="ticket__title">Online Conference Ticket</h2>
-            <span class="ticket__price">free</span>
-            <p class="ticket__info">
-                This ticket gives access to whole online conference on October 20th.
-            </p>
-            <span class="button" title="Buy Now">
-              <span>Buy Now</span>
-              <svg width="11" height="12">
-                <path fill="none" fill-rule="evenodd" stroke="#0086FF" stroke-width="2" d="M1 1l7.358 5L1 11"/>
-              </svg>
-            </span>
-          </a>
-        </article>
+    <section #tickets class="site-content__section">
+      <div class="site-content__wrap">
+          <h1 class="tickets_title">TICKETS</h1>
+          <div class="ticket_card_list">
+            <article class="ticket_card">
+              <a href="https://ti.to/ngrome-conf/NGRome-Conf-MMXX-Online/with/ujp-ev2-cqs"
+              title="Ticket">
+                <div class="ticket_card_header">
+                  <h2 class="ticket_card_title">Online Conference Ticket</h2>
+                  <span class="ticket_card_price">Free</span>
+                </div>
+                <p class="ticket_card_info">
+                  This ticket gives access to whole online conference on October 20th.
+                </p>
+                <button class="ticket_card_button" title="Get tickets">
+                  <span>
+                    Get tickets
+                    <img class="ticket_card_icon" src="./assets/icons/ticket-icon.svg">
+                  </span>
+                </button>
+              </a>
+            </article>
+          </div>
       </div>
-    </div>
-  </section>
+    </section>
   `
 })
 export class TicketsComponent implements OnInit {
