d82e602097831636f14117894744480cabcd64a9
Luciano Murruni <luciano.murruni@gmail.com>
2020-10-14 19:06:32 +0200

    Fix firebase  app-ID, improve SW unregistation

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,13 +6,14 @@ export const environment = {
   production: false,
   eventBriteKey: 'XWRQ7HXFCBECMBFMPN6M',
   firebase: {
-    apiKey: "AIzaSyCEReSoxLo9zXnjrQS0zZL9kiInqSFv4Yc",
-    authDomain: "ngrome-79ce3.firebaseapp.com",
-    databaseURL: "https://ngrome-79ce3.firebaseio.com",
-    projectId: "ngrome-79ce3",
-    storageBucket: "ngrome-79ce3.appspot.com",
-    messagingSenderId: "1003261058692"
-  }
+    apiKey: 'AIzaSyCEReSoxLo9zXnjrQS0zZL9kiInqSFv4Yc',
+    authDomain: 'ngrome-79ce3.firebaseapp.com',
+    databaseURL: 'https://ngrome-79ce3.firebaseio.com',
+    projectId: 'ngrome-79ce3',
+    storageBucket: 'ngrome-79ce3.appspot.com',
+    messagingSenderId: '1003261058692',
+    appId: '1:1003261058692:web:6dd74a58d5f5c6b9beecd1',
+  },
 };
 
 /*
