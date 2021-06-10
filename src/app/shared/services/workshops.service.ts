2d384d299fa72873dd7ddd4ace2b1e7a18ecfc89
Luciano Murruni <luciano.murruni@gmail.com>
2021-06-10 08:46:14 +0200

    add new workshop page

--- src/app/shared/services/workshops.service.ts
+++ src/app/shared/services/workshops.service.ts
@@ -20,95 +20,39 @@ export interface Workshop {
 export class WorkshopsService {
   workshop: Workshop[] = [
     {
-      image: 'https://angularconnect.com/perch/resources/nggirls.png',
-      targetUrl: 'http://ng-girls.org/ngRome-2019/',
-      name: 'Ng Girls Workshop [FREE]',
-      description: 'A short description',
-      fullDescription: `
-       We believe that the IT industry will greatly benefit from bringing more women into technology. \n
-       We want to give you an opportunity to learn how to program and become a programmer.
-       Our plan is to offer a workshop in Rome. <br /><br />
-       <b>Who is it for?</b>
-       <br /><br />
-       Are you a woman interested in learning how to build web applications with the latest technologies?
-       We have good news for you: We are holding a one-day workshop for beginners!
-       <br /><br />
-       It will take place on Sunday, October 6th, at the Teatro Italia in Rome, Italy.
-       We believe that the IT industry will greatly benefit from bringing more women into technology.
-       We want to give you an opportunity to learn how to program and become a programmer.<br><br>
-
-       The workshop is free of charge. Just fill out the <a href="https://forms.gle/gZi9USYY8R6M5C6q8" target="_blank" rel="noopener"><u>application form</u></a>. NGROME is giving discounter tickets for participants of the ngGirls workshop tickets for the <a href="https://www.eventbrite.it/e/ng-rome-mmxix-angular-conference-tickets-54968197358?aff=ebdssbdestsearch" target="_blank" rel="noopener"><u>NG Rome MMXIX</u></a> in Rome, Italy on 7th October.
-       <br><br>Don’t wait too long - you can apply for a pass only until October 1st, 2019!
-       <br>You are welcome to join the ngGirls <a href="http://www.meetup.com/ngGirls/" target="_blank" rel="noopener"><u>meetup group</u></a> to get informed about future events.
-      `,
+      image: 'https://pbs.twimg.com/profile_images/1281665915994746880/aqhVyys1_400x400.jpg',
+      targetUrl: 'https://ti.to/ngrome-conf/ngrome-conf-mmxxi-online-9th-july-2021#schedule',
+      name: ' Stepan Suvorov - Angular Performance Tuning',
+      description: '<b>Duration</b>: 2h\n',
+      fullDescription: ``,
       visible: true,
       location: 'We are hosted at...',
-      start_time: '9:00',
-      end_time: '17:00',
-      twitter: ['AngularGirls', 'MartinaKraus11']
+      start_time: '14:00',
+      end_time: '16:00',
+      twitter: ['stevermeister'],
     },
     {
-      image: 'https://firebasestorage.googleapis.com/v0/b/ngrome-79ce3.appspot.com/o/speaker%2Fdan-john.png?alt=media&token=1c969c7b-5bb8-40ca-9811-1a15bc81f0e4',
-      targetUrl: 'https://www.eventbrite.it/e/ng-rome-mmxix-angular-conference-tickets-54968197358',
-      name: 'Angular Architecture by John Papa and Dan Wahlin',
-      description: 'A short description',
-      fullDescription: `
-        Are you building Angular apps today?
-        Are you following established best practices?
-        How easy will it be to maintain and refactor the application in the future?
-        <br /><br />
-        If you’re starting a new application from scratch, what application architecture should be used?
-        These types of questions are common when building any type of application especially when you’re
-        focused on meeting a deadline. While there’s a lot of information out there about the core
-        concepts of Angular, very little focuses on architectural concepts, best practices,
-        and how to solve some of the more challenging tasks that come up.
-        <br /><br />
-        That’s the purpose of this workshop.
-        <br /><br />
-        <b>Who should attend</b><br />
-        This one day workshop is for developers who are skilled in the basics of Angular
-        but are looking to take the next steps.
-        <br /><br />
-        <b>Prerequisites</b><br />
-        JavaScript skills, Angular experience, a laptop with Node.js
-        LTS installed and the latest version of the Angular CLI
-        Participants can find instructions on how to prepare for the class here:
-        <a href="https://gist.github.com/johnpapa/5968d567c4450ab786f324980ab07b46">
-          https://gist.github.com/johnpapa/5968d567c4450ab786f324980ab07b46
-        </a>
-        <br /><br />
-        <b>What will you learn</b><br />
-        This workshop will provide guidance to help you think through the
-        process of building a solid application architecture with Angular
-        (version 8+) that is easy to refactor and maintain.
-        <br /><br />
-        Some of the topics covered include:
-        <br />- Planning your application architecture
-        <br />- Structuring features and modules
-        <br />- Optimizing routes
-        <br />- Component communication
-        <br />- State management
-        <br /><br />
-        Come and engage in a day of in-depth discussions and hands-on examples of
-        different architecture concepts and best practices that
-        can be applied to your Angular applications.
-      `,
+      image: 'https://pbs.twimg.com/profile_images/1364683651238273038/C-m4fT-Z_400x400.jpg',
+      targetUrl: 'https://ti.to/ngrome-conf/ngrome-conf-mmxxi-online-9th-july-2021#schedule',
+      name: 'Gerard Sans - Fullstack GraphQL using Angular v12',
+      description: 'Duration 2h',
+      fullDescription: ``,
       visible: true,
       location: 'We are hosted at...',
       start_time: '9:00',
       end_time: '17:00',
-      twitter: ['John_Papa', 'DanWahlin']
+      twitter: ['John_Papa'],
     },
   ];
 
-  getWorkshops(filter?): Workshop[]{
-      let workshops = this.workshop;
-      //console.log(filter);
-      if (filter){
-        workshops = this.workshop.filter( (e) => {
-          return (e.name === filter.name);
-        });
-      }
-      return workshops;
+  getWorkshops(filter?): Workshop[] {
+    let workshops = this.workshop;
+    //console.log(filter);
+    if (filter) {
+      workshops = this.workshop.filter((e) => {
+        return e.name === filter.name;
+      });
+    }
+    return workshops;
   }
 }
