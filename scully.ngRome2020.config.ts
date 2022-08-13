0e01356738a9bdaaf0cb04b69661fc555beac50d
Luciano Murruni <luciano.murruni@gmail.com>
2022-08-13 14:10:23 +0200

    update scully version

--- scully.ngRome2020.config.ts
+++ scully.ngRome2020.config.ts
@@ -1,8 +1,10 @@
 import { ScullyConfig } from '@scullyio/scully';
+
 export const config: ScullyConfig = {
-  projectRoot: "./src",
-  projectName: "ngRome2020",
-  outDir: './dist/static',
-  routes: {
-  }
+  projectRoot: './src',
+  projectName: 'ngRome2020',
+  distFolder: './dist/[PROJECT_NAME]', // output directory of your Angular build artifacts
+  outDir: './dist/static', // directory for scully build artifacts
+  defaultPostRenderers: [],
+  routes: {},
 };
