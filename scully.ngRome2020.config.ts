d1b4e13e7f9d94b44b3814efc7f4dd5e3fb75ec4
Luciano <luciano.murruni@gmail.com>
2022-08-13 22:47:50 +0200

    Update scully to version 2.1.x (#179)
    * update scully version
    
    * upgrade to scully 2.1.x
    
    * remove scully.log file from tracking

--- scully.ngRome2020.config.ts
+++ scully.ngRome2020.config.ts
@@ -1,8 +1,11 @@
 import { ScullyConfig } from '@scullyio/scully';
+import '@scullyio/scully-plugin-puppeteer';
+
 export const config: ScullyConfig = {
-  projectRoot: "./src",
-  projectName: "ngRome2020",
-  outDir: './dist/static',
-  routes: {
-  }
+  projectRoot: './src',
+  projectName: 'ngRome2020',
+  distFolder: './dist/ngRome2020', // output directory of your Angular build artifacts
+  outDir: './dist/static', // directory for scully build artifacts
+  defaultPostRenderers: ['routeRenderer'],
+  routes: {},
 };
