74766316817bdd3bdd310093191db0b7beae54a6
Luciano Murruni <luciano.murruni@gmail.com>
2022-08-13 16:24:17 +0200

    upgrade to scully 2.1.x

--- scully.ngRome2020.config.ts
+++ scully.ngRome2020.config.ts
@@ -1,10 +1,11 @@
 import { ScullyConfig } from '@scullyio/scully';
+import '@scullyio/scully-plugin-puppeteer';
 
 export const config: ScullyConfig = {
   projectRoot: './src',
   projectName: 'ngRome2020',
-  distFolder: './dist/[PROJECT_NAME]', // output directory of your Angular build artifacts
+  distFolder: './dist/ngRome2020', // output directory of your Angular build artifacts
   outDir: './dist/static', // directory for scully build artifacts
-  defaultPostRenderers: [],
+  defaultPostRenderers: ['routeRenderer'],
   routes: {},
 };
