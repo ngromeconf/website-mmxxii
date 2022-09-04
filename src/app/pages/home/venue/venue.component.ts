31d00557631cda1d92da9d0904b5a61aff1fd813
Luciano Murruni <luciano.murruni@gmail.com>
2022-09-04 17:05:51 +0200

    add schedule in home

--- src/app/pages/home/venue/venue.component.ts
+++ src/app/pages/home/venue/venue.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+interface SCHEDULE {
+  time: string;
+  event: string;
+}
 @Component({
   selector: 'ngrome-venue',
   template: `
@@ -11,31 +15,56 @@ import { Component, OnInit } from '@angular/core';
           <div class="site-content__intro__info">
           <p class="">NG-Rome MMXXII will take place at <a [routerLink]="['/info']">Spazio Novecento</a> in the heart of EUR, an historical neighborhood two steps away from the city center of Rome.</p>
           </div>
+          <div class="site-venue"></div>
         </div>
-        <div class="site-venue"></div>
       </div>
-      <!-- <div class="site-content__wrap">
+      <div class="site-content__wrap">
         <h1 class="tickets_title">Agenda</h1>
-        <div class="ticket_card_list">
+        <div class="site-content__intro_info">
           <p>One track, one day, filled with talks of the Angular engineers who build the most performant apps and web experiences.</p>
+
+          <div class="timeline--container">
+            <div class="list">
+              <ul class="timeline">
+                <li class="event" *ngFor="let item of schedule" [attr.data-date]="item.time">
+                  <h3>{{item.event}}</h3>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
-      </div> -->
+      </div>
     </section>
   `,
-  styles: [
-    `
-
-      section {
-        margin-top: 40px;
-      }
-      img {
-        max-width: 106px;
-      }
-
-    `,
-  ],
+  styleUrls: ['./venue.component.scss'],
 })
 export class VenueComponent implements OnInit {
+  schedule: SCHEDULE[] = [
+    {'time': '8:00am',	'event': 'Registration & Coffee'},
+    {'time': '9:00am',	'event': 'Opening Remarks'},
+    {'time': '9:10am',	'event': 'Opening Keynote'},
+    {'time': '9:50am',	'event': 'Talk 1'},
+    {'time': '10:15am',	'event': 'Lightning Talk 1'},
+    {'time': '10:25am',	'event': 'Sponsor Talk Platinum'},
+    {'time': '10:30am',	'event': 'Panel 1'},
+    {'time': '10:50am',	'event': 'Coffee Break'},
+    {'time': '11:15am',	'event': 'Talk 2'},
+    {'time': '11:40am',	'event': 'Lightning Talks 2'},
+    {'time': '11:50am',	'event': 'Talk 3'},
+    {'time': '12:15pm',	'event': 'Sponsor Talk Platinum'},
+    {'time': '12:20pm',	'event': 'Panel 2'},
+    {'time': '12:40pm',	'event': 'Lunch'},
+    {'time': '1:40pm',	'event': 'Talk 4'},
+    {'time': '2:05pm',	'event': 'Lightning Talks 3'},
+    {'time': '2:15pm',	'event': 'Talk 5'},
+    {'time': '2:40pm',	'event': 'Panel 3'},
+    {'time': '3:00pm',	'event': 'Coffee Break'},
+    {'time': '3:25pm',	'event': 'Talk 6'},
+    {'time': '3:50pm',	'event': 'Closing Keynote'},
+    {'time': '4:30pm',	'event': 'Final Panel'},
+    {'time': '4:50pm',	'event': 'Closing Remarks'},
+    {'time': '5:00pm',	'event': 'After Party'},
+  ];
   constructor() {}
   ngOnInit() {}
 }
