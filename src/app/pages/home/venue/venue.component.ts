21a49c116cd7a43be04f57ea497d93bd7d7b00d3
Luciano Murruni <luciano.murruni@gmail.com>
2022-10-07 21:11:08 +0200

    accomodation and speacker suggestions

--- src/app/pages/home/venue/venue.component.ts
+++ src/app/pages/home/venue/venue.component.ts
@@ -39,32 +39,6 @@ interface SCHEDULE {
   styleUrls: ['./venue.component.scss'],
 })
 export class VenueComponent implements OnInit {
-  schedule: SCHEDULE[] = [
-    {'time': '8:00am',	'event': 'Registration & Coffee'},
-    {'time': '9:00am',	'event': 'Opening Remarks'},
-    {'time': '9:10am',	'event': 'Opening Keynote'},
-    {'time': '9:50am',	'event': 'Talk 1'},
-    {'time': '10:15am',	'event': 'Lightning Talk 1'},
-    {'time': '10:25am',	'event': 'Sponsor Talk: Almaviva Platinum Sponsor'},
-    {'time': '10:30am',	'event': 'Panel 1'},
-    {'time': '10:50am',	'event': 'Coffee Break'},
-    {'time': '11:15am',	'event': 'Talk 2'},
-    {'time': '11:40am',	'event': 'Lightning Talks 2'},
-    {'time': '11:50am',	'event': 'Talk 3'},
-    {'time': '12:15pm',	'event': 'Sponsor Talk Platinum'},
-    {'time': '12:20pm',	'event': 'Panel 2'},
-    {'time': '12:40pm',	'event': 'Lunch'},
-    {'time': '1:40pm',	'event': 'Talk 4'},
-    {'time': '2:05pm',	'event': 'Lightning Talks 3'},
-    {'time': '2:15pm',	'event': 'Talk 5'},
-    {'time': '2:40pm',	'event': 'Panel 3'},
-    {'time': '3:00pm',	'event': 'Coffee Break'},
-    {'time': '3:25pm',	'event': 'Talk 6'},
-    {'time': '3:50pm',	'event': 'A Fistful of Angulars by Brian Love and Mike Ryan'},
-    {'time': '4:30pm',	'event': 'Final Panel'},
-    {'time': '4:50pm',	'event': 'Closing Remarks'},
-    {'time': '5:00pm',	'event': 'After Party'},
-  ];
   constructor() {}
   ngOnInit() {}
 }
